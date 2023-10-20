8c80006d1c42d7f36d6aedaab5a9d3b8b7add8eb
ben9964 <b.masters@se20.qmul.ac.uk>
2023-10-20 09:52:31 +0100

    clone character properly

--- Assets/Players/Scripts/LoadCharacter.cs
+++ Assets/Players/Scripts/LoadCharacter.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class LoadCharacter : MonoBehaviour
 {
     public Transform spawnPos;
+    public Vector3 offset;
 
     void Start()
     {
         int selectedCharacter = PlayerPrefs.GetInt("character");
         GameObject character = Characters.Instance.GetCharacter(selectedCharacter);
-        GameObject clone = Instantiate(character, spawnPos.position, Quaternion.identity);
+        GameObject clone = Instantiate(character, spawnPos.position+offset, Quaternion.identity, GameObject.FindWithTag("Player").transform);
+        character.SetActive(false);
     }
 }
