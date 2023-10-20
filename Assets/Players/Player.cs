14f89ee0ae261431924962fbda5c18c1eda9b6a1
ben9964 <b.masters@se20.qmul.ac.uk>
2023-10-20 10:59:27 +0100

    Character attributes

---
+++ Assets/Players/Player.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using StarterAssets;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Players
+{
+    public abstract class Player : MonoBehaviour
+    {
+        private float _health;
+        public float maxHealth;
+        public float speed;
+
+        private void Start()
+        {
+            this._health = maxHealth;
+            var controller = this.gameObject.GetComponentInParent<FirstPersonController>(false);
+            if (controller != null)
+            {
+                controller.MoveSpeed = speed;
+            }
+
+            if (IsInGame())
+            {
+                StartCoroutine(killPlayer());
+            }
+        }
+
+        private static bool IsInGame()
+        {
+            return !SceneManager.GetActiveScene().name.Equals("CharacterSelect");
+        }
+
+        public abstract void Attack();
+
+        public void TakeDamage(float amount)
+        {
+            this._health -= amount;
+            if (this._health <= 0)
+            {
+                die();
+            }
+        }
+
+        private void die()
+        {
+            GameObject.FindWithTag("death").SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        public float GetHealth()
+        {
+            return this._health;
+        }
+
+        public float GetMaxHealth()
+        {
+            return this.maxHealth;
+        }
+
+        public float GetSpeed()
+        {
+            return this.speed;
+        }
+
+        private IEnumerator killPlayer()
+        {
+            yield return new WaitForSeconds(5);
+            
+            die();
+        }
+    }
+}
---
+++ Assets/Players/impl/DamagePlayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Players.impl
+{
+    public class DamagePlayer : Player
+    {
+
+        public override void Attack()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
