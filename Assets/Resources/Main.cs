c033179c3851f34a27096c6760ce283777945ded
ben9964 <b.masters@se20.qmul.ac.uk>
2023-10-23 16:08:31 +0100

    CharacterDB Loading first

--- Assets/Resources/Main.cs
+++ Assets/Resources/Main.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    // This bit of code runs before any scenes load and allows us to initialize global utilities such as the character
+    // database without needing to put an object in every scene OR go through the character select every time we want to
+    // test a given scene.
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     public static void LoadMain()
     {
