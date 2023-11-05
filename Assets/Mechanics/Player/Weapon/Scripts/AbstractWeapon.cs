021ae2ed4562be096594fb87857bb682326eaa79
ben9964 <35692789+ben9964@users.noreply.github.com>
2023-11-05 18:51:29 +0000

    Add Shoot Cooldown

--- Assets/Mechanics/Player/Weapon/Scripts/AbstractWeapon.cs
+++ Assets/Mechanics/Player/Weapon/Scripts/AbstractWeapon.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public abstract class AbstractWeapon : MonoBehaviour
@@ -8,13 +10,22 @@ public abstract class AbstractWeapon : MonoBehaviour
     public float damage;
     public Transform muzzle;
     public GameObject projectilePrefab;
+    public float shootCooldown;
+    private bool canShoot = true;
 
     public virtual void Use()
     {
+        if (!canShoot) return;
+        
         GameObject projectileObj = Instantiate(this.projectilePrefab, GetMuzzle().position, quaternion.identity);
         AbstractProjectile clazz = projectileObj.GetComponent<AbstractProjectile>();
         clazz.SetShooter(GameObject.FindWithTag("Player").GetComponentInChildren<AbstractPlayer>());
         clazz.SetWeapon(this);
+        canShoot = false;
+        this.Invoke(() =>
+        {
+            canShoot = true;
+        }, shootCooldown);
     }
 
     public float GetDamage()
